57786ca7cc52e90023943146eca1126e7cdb0e02
Luke Finney <lucaswfinney@gmail.com>
2017-12-26 13:49:26 -0700

    Serve static assets

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
-// eslint-disable-next-line no-unused-vars
-const path = require('path');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
 
-const bodyParser = require('body-parser');
+const database = require('knex')(configuration);
 
 const app = express();
+const bodyParser = require('body-parser');
+
+const requireHTTPS = (request, response, next) => {
+  if (request.header('x-forwarded-proto') !== 'https') {
+    return response.redirect(`https://${request.header('host')}${request.url}`);
+  }
+  next();
+};
+if (process.env.NODE_ENV === 'production') { app.use(requireHTTPS); }
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
