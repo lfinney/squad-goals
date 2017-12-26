770764e1c3e7912f31f02e0c4958946d1738ed52
Luke Finney <lucaswfinney@gmail.com>
2017-12-26 13:51:01 -0700

    linter update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
+
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
-
 const database = require('knex')(configuration);
 
 const app = express();
