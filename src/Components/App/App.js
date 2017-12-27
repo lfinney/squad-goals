b451089f886228f1fd8665387f966c7293820e30
Luke Finney <lucaswfinney@gmail.com>
2017-12-27 11:22:38 -0700

    Wire up Welcome component to App componenet and reorganize files

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
+import Welcome from '../Welcome/Welcome';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Welcome />
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
---
+++ src/Components/Welcome/Welcome.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import './Welcome.css';
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div className="Welcome">
+        HELLO!
+      </div>
+    );
+  }
+}
+
+export default Welcome;
