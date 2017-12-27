37e332fc8d01ee302723ab9196622c54f865450e
johnmboudreaux <jhnbdrx@gmail.com>
2017-12-27 15:01:35 -0700

    added browser router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,14 +8,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Welcome />
-        <Navigation />
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/Navigation" component={Navigation} />
       </div>
     );
   }
--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './Welcome.css';
 
-class Welcome extends Component {
+class Welcome extends React.Component {
   render() {
     return (
       <div className="Welcome">
