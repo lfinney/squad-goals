dee09e766017f3631603d098c9bf4a9475c33d51
Luke Finney <lucaswfinney@gmail.com>
2017-12-27 11:25:47 -0700

    Wire Navigation Component to App

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import './Navigation.css';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div className="Navigation">
+        IM A NAVIGATOR!
+      </div>
+    );
+  }
+}
+
+export default Navigation;
