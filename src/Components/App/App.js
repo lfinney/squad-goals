3c032f498b8e3d3693acf41f1dc0a1b01e9b5003
johnmboudreaux <jhnbdrx@gmail.com>
2017-12-27 21:16:51 -0700

    added squads component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import Welcome from '../Welcome/Welcome';
 import Navigation from '../Navigation/Navigation';
 import Dashboard from '../Dashboard/Dashboard';
+import Squads from '../Squads/Squads';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Route exact path="/" component={Welcome} />
         <Route path="/Navigation" component={Navigation} />
         <Route path="/Dashboard" component={Dashboard} />
+        <Route path="/Squads" component={Squads} />
       </div>
     );
   }
---
+++ src/Components/Squads/Squads.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Squads extends Component {
+  render() {
+    return (
+      <div className="squads">
+        <h1>Squads</h1>
+      </div>
+    );
+  }
+}
+
+export default Squads;
