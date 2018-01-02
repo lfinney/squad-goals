aff33c02da5488969f95e2d82fc3b6ef8f66852d
Luke Finney <lucaswfinney@gmail.com>
2018-01-02 13:40:38 -0700

    Write seed functions for test environment

--- db/seeds/test/users_test.js
+++ db/seeds/test/users_test.js
@@ -2,6 +2,9 @@ const challengesData = require('../../../data/challenges_data_test');
 const commentsData = require('../../../data/comments_data_test');
 const conversationsData = require('../../../data/conversations_data_test');
 const usersData = require('../../../data/users_data_test');
+const squadData = require('../../../data/squad_data_test');
+const userSquadData = require('../../../data/users_squads_data_test');
+const usersChallengesData = require('../../../data/users_challenges_data_test');
 
 const createUser = (knex, user) => {
   return knex('users').insert(user);
@@ -19,8 +22,23 @@ const createChallenges = (knex, challenge) => {
   return knex('challenges').insert(challenge);
 };
 
+const createSquads = (knex, squad) => {
+  return knex('squads').insert(squad);
+};
+
+const createUserSquad = (knex, userSquad) => {
+  return knex('users_squads').insert(userSquad);
+};
+
+const createUserChallenges = (knex, userChallenge) => {
+  return knex('users_challenges').insert(userChallenge);
+};
+
 exports.seed = (knex, Promise) => {
-  return knex('challenges').del()
+  return knex('users_challenges').del()
+    .then(() => knex('users_squads').del())
+    .then(() => knex('squads').del())
+    .then(() => knex('challenges').del())
     .then(() => knex('comments').del())
     .then(() => knex('conversations').del())
     .then(() => knex('users').del())
@@ -55,5 +73,29 @@ exports.seed = (knex, Promise) => {
         challengesPromises.push(createChallenges(knex, challenge));
       });
       return Promise.all(challengesPromises);
+    })
+    .then(() => {
+      const squadPromises = [];
+
+      squadData.forEach((squad) => {
+        squadPromises.push(createSquads(knex, squad));
+      });
+      return Promise.all(squadPromises);
+    })
+    .then(() => {
+      const userSquadPromises = [];
+
+      userSquadData.forEach((userSquad) => {
+        userSquadPromises.push(createUserSquad(knex, userSquad));
+      });
+      return Promise.all(userSquadPromises);
+    })
+    .then(() => {
+      const userChallengesPromises = [];
+
+      usersChallengesData.forEach((userChallenge) => {
+        userChallengesPromises.push(createUserChallenges(knex, userChallenge));
+      });
+      return Promise.all(userChallengesPromises);
     });
 };
