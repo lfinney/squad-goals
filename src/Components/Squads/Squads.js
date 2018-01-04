45c1d9b67097f5e5595f5cb90b925f52b5ed0ae7
johnmboudreaux <jhnbdrx@gmail.com>
2018-01-04 10:46:58 -0700

    added variables for css and changed some outin squads component

--- src/Components/Squads/Squads.js
+++ src/Components/Squads/Squads.js
@@ -7,6 +7,9 @@ class Squads extends Component {
         <h2 className="squads-body-title">Squads</h2>
         <div className="squads-body-info">
           <p>Squad Title</p>
+          <p>Competitors</p>
+          <p>Squad Challenges</p>
+          <input type="button" value="Leave" />
         </div>
       </div>
     );
