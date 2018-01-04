9787c47d38d0a7a9686e1b9f1b9e22bd02622e8b
johnmboudreaux <jhnbdrx@gmail.com>
2018-01-03 20:39:18 -0700

    added html and styling for challenges into squads.js component

--- src/Components/Squads/Squads.js
+++ src/Components/Squads/Squads.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 class Squads extends Component {
   render() {
     return (
-      <div className="squads">
-        <h1>Squads</h1>
+      <div className="squads-body">
+        <h2 className="squads-body-title">Squads</h2>
+        <div className="squads-body-info">
+          <p>Challenge Title</p>
+          <p>Challenge Description </p>
+          <p>Challenge Time </p>
+          <p>Challenge Points </p>
+          <p>Challenge Creator </p>
+        </div>
       </div>
     );
   }
