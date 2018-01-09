bf9a239897c6d70ce8383ce11078b4ea92704cef
johnmboudreaux <jhnbdrx@gmail.com>
2018-01-08 19:08:52 -0700

    squadcard snapshot passing

--- src/Components/SquadCard/SquadCard.test.js
+++ src/Components/SquadCard/SquadCard.test.js
@@ -1,10 +1,24 @@
 /* eslint-disable */
-
+import { shallow } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Welcome from './Welcome';
+import SquadCard from './SquadCard';
+import squad from '../../../data/test/squad_data_test';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Welcome />, div);
+describe('SquadCard snapshot', () => {
+  it('should always match the snapshot', () => {
+    const wrapper = shallow(<SquadCard
+      squad={squad}
+    />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+
+  // it('renders without crashing', () => {
+  //   const div = document.createElement('div');
+  //   ReactDOM.render(<SquadCard
+  //     squad={squad}
+  //   />, div);
+  // });
 });
