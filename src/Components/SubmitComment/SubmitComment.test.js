4fd453c135933ed1c6c2c757cb10d796487eb62b
johnmboudreaux <jhnbdrx@gmail.com>
2018-01-08 20:40:42 -0700

    submitCommetn snapshot and render tests passing

--- src/Components/SubmitComment/SubmitComment.test.js
+++ src/Components/SubmitComment/SubmitComment.test.js
@@ -1,10 +1,19 @@
-/* eslint-disable */
-
+import { shallow } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SubmitComment from './SubmitComment';
+// import goal from '../../../data/test/goals_data_test';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SubmitComment />, div);
+describe('SubmitComment snapshot', () => {
+  it('should always match the snapshot', () => {
+    const wrapper = shallow(<SubmitComment />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<SubmitComment />, div);
+  });
 });
