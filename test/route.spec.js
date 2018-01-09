58e2e074b4b19da23cdd53d7e0d1d84355a3dc1c
Luke Finney <lucaswfinney@gmail.com>
2018-01-08 20:20:37 -0700

    Write tests for DELETE api/v1/goals/:id

--- test/route.spec.js
+++ test/route.spec.js
@@ -426,4 +426,21 @@ describe('API Routes', () => {
         });
     });
   });
+
+  describe('DELETE /api/v1/goals/:id', () => {
+    it('should delete a specific goal', (done) => {
+      chai.request(server)
+        .delete('/api/v1/goals/1')
+        .end((error, response) => {
+          response.should.have.status(204);
+          response.body.should.be.a('object');
+          chai.request(server)
+            .get('/api/v1/goals/1')
+            .end((deleteError, deleteResponse) => {
+              deleteResponse.should.have.status(404);
+              done();
+            });
+        });
+    });
+  });
 });
