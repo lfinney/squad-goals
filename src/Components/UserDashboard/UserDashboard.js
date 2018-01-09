97c3d154387735a69e52a09b6c453dda18c90100
Luke Finney <lucaswfinney@gmail.com>
2018-01-09 09:26:22 -0700

    Update heights and widths on components

--- src/Components/UserDashboard/UserDashboard.js
+++ src/Components/UserDashboard/UserDashboard.js
@@ -107,94 +107,95 @@ class UserDashboard extends Component {
     console.log(this.state);
     return (
       <div className="dashboard-container">
-        <h1>Squad Goals</h1>
-        <div className="user-info dashboard-header">
-          <h2 className="user-name">{this.state.activeUser}</h2>
-          <h2 className="user-points">{this.state.points} Points</h2>
-          <input
-            onClick={() => {
-            this.deleteAccount(this.state.activeUserId);
-          }}
-            className="nav-button"
-            type="button"
-            value="Delete Account"
-          />
-        </div>
-        <div className="dashboard-header">
-          <input
-            className="nav-button"
-            onClick={() => {
-            this.showContent('user-squads');
-          }}
-            type="button"
-            value="My Squads"
-          />
-          <input
-            className="nav-button"
-            onClick={() => {
-            this.showContent('user-goals');
-          }}
-            type="button"
-            value="My Goals"
-          />
-          <input
-            className="nav-button"
-            onClick={() => {
-            this.showContent('all-squads');
-          }}
-            type="button"
-            value="All Squads"
-          />
-          <input
-            className="nav-button"
-            onClick={() => {
-            this.showContent('all-goals');
-          }}
-            type="button"
-            value="All Goals"
-          />
-          <Link
-            className="nav-button"
-            to={{
-            pathname: '/CreateSquads',
-            state: { userId: this.state.activeUserId },
-          }}
-          >
+        <div className="content-container">
+          <div className="user-info dashboard-header">
+            <h2 className="user-name">{this.state.activeUser}</h2>
+            <h2 className="user-points">{this.state.points} Points</h2>
+            <input
+              onClick={() => {
+              this.deleteAccount(this.state.activeUserId);
+            }}
+              className="nav-button"
+              type="button"
+              value="Delete Account"
+            />
+          </div>
+          <div className="dashboard-header">
+            <input
+              className="nav-button"
+              onClick={() => {
+              this.showContent('user-squads');
+            }}
+              type="button"
+              value="My Squads"
+            />
+            <input
+              className="nav-button"
+              onClick={() => {
+              this.showContent('user-goals');
+            }}
+              type="button"
+              value="My Goals"
+            />
+            <input
+              className="nav-button"
+              onClick={() => {
+              this.showContent('all-squads');
+            }}
+              type="button"
+              value="All Squads"
+            />
+            <input
+              className="nav-button"
+              onClick={() => {
+              this.showContent('all-goals');
+            }}
+              type="button"
+              value="All Goals"
+            />
+            <Link
+              className="nav-button"
+              to={{
+              pathname: '/CreateSquads',
+              state: { userId: this.state.activeUserId },
+            }}
+            >
           New Squad
-          </Link>
+            </Link>
+          </div>
+          {
+            this.state.displayComponent === 'user-squads' &&
+            <SquadsContainer
+              userId={this.state.activeUserId}
+              leaveGroup={this.leaveGroup}
+              squadData={this.state.userSquadData}
+            />
+          }
+          {
+            this.state.displayComponent === 'all-squads' &&
+            <SquadsContainer
+              userId={this.state.activeUserId}
+              leaveGroup={this.leaveGroup}
+              squadData={this.state.allSquadData}
+            />
+          }
+          {
+            this.state.displayComponent === 'user-goals' &&
+            <GoalsContainer
+              userId={this.state.activeUserId}
+              leaveGroup={this.leaveGroup}
+              goalData={this.state.userGoalData}
+            />
+          }
+          {
+            this.state.displayComponent === 'all-goals' &&
+            <GoalsContainer
+              userId={this.state.activeUserId}
+              leaveGroup={this.leaveGroup}
+              goalData={this.state.allGoalData}
+            />
+          }
         </div>
-        {
-          this.state.displayComponent === 'user-squads' &&
-          <SquadsContainer
-            userId={this.state.activeUserId}
-            leaveGroup={this.leaveGroup}
-            squadData={this.state.userSquadData}
-          />
-        }
-        {
-          this.state.displayComponent === 'all-squads' &&
-          <SquadsContainer
-            userId={this.state.activeUserId}
-            leaveGroup={this.leaveGroup}
-            squadData={this.state.allSquadData}
-          />
-        }
-        {
-          this.state.displayComponent === 'user-goals' &&
-          <GoalsContainer
-            userId={this.state.activeUserId}
-            leaveGroup={this.leaveGroup}
-            goalData={this.state.userGoalData}
-          />
-        }
-        {
-          this.state.displayComponent === 'all-goals' &&
-          <GoalsContainer
-            userId={this.state.activeUserId}
-            leaveGroup={this.leaveGroup}
-            goalData={this.state.allGoalData}
-          />
-        }
       </div>
     );
   }
