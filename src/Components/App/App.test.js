256e413be86fdd43680c7116cf17edefc6d7d1df
johnmboudreaux <jhnbdrx@gmail.com>
2018-01-08 17:51:13 -0700

    app snapshot passed

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,10 +1,11 @@
-/* eslint-disable */
-
+import { shallow } from 'enzyme';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import MainApp from './App.js';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('MainApp snapshot', () => {
+  it('should always match the snapshot', () => {
+    const wrapper = shallow(<MainApp />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
 });
