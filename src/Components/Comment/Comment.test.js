14531cfedcb268d356a9c3de9d45010938e9be5c
johnmboudreaux <jhnbdrx@gmail.com>
2018-01-08 18:09:17 -0700

    conversaion test passing

--- src/Components/Comment/Comment.test.js
+++ src/Components/Comment/Comment.test.js
@@ -1,10 +1,23 @@
-/* eslint-disable */
-
+import { shallow } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GoalsContainer from './GoalsContainer';
+import Comment from './Comment';
+import commentData from '../../../data/test/comments_data_test';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<GoalsContainer />, div);
+describe('MainApp snapshot', () => {
+  it('should always match the snapshot', () => {
+    const wrapper = shallow(<Comment
+      commentData={commentData}
+    />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Comment
+      commentData={commentData}
+    />, div);
+  });
 });
