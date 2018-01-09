24b414423160430a8d5933c06ed5d24a5194c421
johnmboudreaux <jhnbdrx@gmail.com>
2018-01-08 20:50:26 -0700

    added render test to app

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,5 +1,7 @@
 import { shallow } from 'enzyme';
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import MainApp from './App.js';
 
 describe('MainApp snapshot', () => {
@@ -7,5 +9,10 @@ describe('MainApp snapshot', () => {
     const wrapper = shallow(<MainApp />);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<BrowserRouter><MainApp /></BrowserRouter>, div);
   });
 });
