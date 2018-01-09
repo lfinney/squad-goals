192fc6495aef7c355e6845befb4d66a89271f57e
johnmboudreaux <jhnbdrx@gmail.com>
2018-01-08 19:04:42 -0700

    navigation snapshot passing

--- src/Components/GoalCard/GoalCard.test.js
+++ src/Components/GoalCard/GoalCard.test.js
@@ -1,10 +1,23 @@
-/* eslint-disable */
-
+import { shallow } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Welcome from './Welcome';
+import GoalCard from './GoalCard';
+import goal from '../../../data/test/goals_data_test';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Welcome />, div);
+describe('MainApp snapshot', () => {
+  it('should always match the snapshot', () => {
+    const wrapper = shallow(<GoalCard
+      goal={goal}
+    />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<GoalCard
+      goal={goal}
+    />, div);
+  });
 });
--- src/Components/Navigation/Navigation.test.js
+++ src/Components/Navigation/Navigation.test.js
@@ -1,10 +1,19 @@
-/* eslint-disable */
-
+import { shallow } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Navigation from './Navigation';
+// import goal from '../../../data/test/goals_data_test';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('Navigation snapshot', () => {
+  it('should always match the snapshot', () => {
+    const wrapper = shallow(<Navigation />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+
+  // it('renders without crashing', () => {
+  //   const div = document.createElement('div');
+  //   ReactDOM.render(<Navigation />, div);
+  // });
 });
