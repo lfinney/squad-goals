9386d6033ee9e965dcb13a556583b5aa788e34ed
Luke Finney <lucaswfinney@gmail.com>
2018-01-08 20:32:44 -0700

    Write tests for DELETE api/v1/comments

--- test/route.spec.js
+++ test/route.spec.js
@@ -473,4 +473,21 @@ describe('API Routes', () => {
         });
     });
   });
+
+  describe('DELETE /api/v1/comments/:id', () => {
+    it('should delete a specific comment', (done) => {
+      chai.request(server)
+        .delete('/api/v1/comments/1')
+        .end((error, response) => {
+          response.should.have.status(204);
+          response.body.should.be.a('object');
+          chai.request(server)
+            .get('/api/v1/comments/1')
+            .end((deleteError, deleteResponse) => {
+              deleteResponse.should.have.status(404);
+              done();
+            });
+        });
+    });
+  });
 });
